bebfd3d319f751b556ffb09ec5d6685f04eb140c
samanthamarberger <slatham04@gmail.com>
2023-05-15 15:12:42 -0600

    Migrated muscle groups table

--- db/migrate/20230515202538_create_muscle_groups.rb
+++ db/migrate/20230515202538_create_muscle_groups.rb
@@ -1,4 +1,9 @@
 class CreateMuscleGroups < ActiveRecord::Migration[6.1]
   def change
+    create_table :muscle_groups do |t|
+      t.string :name
+      t.string :image_url
+      t.timestamps
+    end
   end
 end
