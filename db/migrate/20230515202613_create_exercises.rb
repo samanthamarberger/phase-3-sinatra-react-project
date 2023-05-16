00831acf827a49e2c0e6ac7d310b270aed566646
samanthamarberger <slatham04@gmail.com>
2023-05-16 15:12:59 -0600

    Migrated exercises and stretches table

--- db/migrate/20230515202613_create_exercises.rb
+++ db/migrate/20230515202613_create_exercises.rb
@@ -1,4 +1,10 @@
 class CreateExercises < ActiveRecord::Migration[6.1]
   def change
+    create_table :exercises do |t|
+      t.string :name
+      t.string :image_url
+      t.string :how_to_do
+      t.timestamps
+    end
   end
 end
--- db/migrate/20230515202625_create_stretches.rb
+++ db/migrate/20230515202625_create_stretches.rb
@@ -1,4 +1,10 @@
 class CreateStretches < ActiveRecord::Migration[6.1]
   def change
+    create_table :stretches do |t|
+      t.string :name
+      t.string :image_url
+      t.string :how_to_do
+      t.timestamps
+    end
   end
 end
