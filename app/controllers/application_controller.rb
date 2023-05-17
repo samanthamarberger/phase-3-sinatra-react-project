b9807c263caf58750ec0c57b31941a6a858d8a74
samanthamarberger <slatham04@gmail.com>
2023-05-17 12:21:52 -0600

    wrote and tested all get controllers (for now)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,24 @@ class ApplicationController < Sinatra::Base
     "Hello World"
   end
 
-  get "/muscle_groups" do
+  get '/muscle_groups' do
     muscle_groups = MuscleGroup.all 
     muscle_groups.to_json
   end
 
+  get '/muscle_groups/:id' do
+    muscle_group = MuscleGroup.find(params[:id])
+    muscle_group.to_json
+  end
+
+  get '/exercises' do
+    exercises = Exercise.all
+    exercises.to_json
+  end
+
+  get '/stretches' do
+    stretches = Stretch.all
+    stretches.to_json
+  end
 
 end
