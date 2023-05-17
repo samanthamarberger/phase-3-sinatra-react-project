0941f5f98eb316a14e34bda9bed1ac0d040b0ff6
samanthamarberger <slatham04@gmail.com>
2023-05-17 12:35:39 -0600

    Added and tested Post controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,4 +26,32 @@ class ApplicationController < Sinatra::Base
     stretches.to_json
   end
 
+  post '/muscle_groups' do
+    muscle_group = MuscleGroup.create(
+      name: params[:name],
+      image_url: params[:image_url]
+    )
+    muscle_group.to_json
+  end
+
+  post '/exercises' do 
+    exercise = Exercise.create(
+      name: params[:name],
+      image_url: params[:image_url],
+      how_to_do: params[:how_to_do],
+      muscle_group_id: params[:muscle_group_id]
+    )
+    exercise.to_json
+  end
+
+  post '/stretches' do 
+    stretch = Stretch.create(
+      name: params[:name],
+      image_url: params[:image_url],
+      how_to_do: params[:how_to_do],
+      muscle_group_id: params[:muscle_group_id]
+    )
+    stretch.to_json
+  end
+
 end
