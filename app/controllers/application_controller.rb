a7bc59cece53f38dace2a87e12dddec623522feb
samanthamarberger <slatham04@gmail.com>
2023-05-17 12:15:08 -0600

    Wrote up first get requested, tested on postman and works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,14 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get '/' do
+    "Hello World"
   end
+
+  get "/muscle_groups" do
+    muscle_groups = MuscleGroup.all 
+    muscle_groups.to_json
+  end
+
 
 end
